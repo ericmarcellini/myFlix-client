948c223150664c9c5f09d9638d23561c650ffea5
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-06-30 10:49:24 +0200

    index html, jsx and scss were created. gitignore working properly

---
+++ README.md
@@ -0,0 +1,4 @@
+Using React, build the client-side for an application called myFlix based on its existing
+server-side code (REST API and database).
+
+MERN Stack
---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// import statement to indicate that you need to bundle '/.index.scss'
+import './index.scss';
+
+// main compoment
+
+class MyFlixApplication extends React.Component {
+    render(){
+        return (
+            <div className="my-flix">
+                <div>Good morning</div>
+            </div>
+        );
+    }
+}
+
+// finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// tells react to render your app in the root DOM element
+ReractDom.render(React.createElement(MyFlixApplication), container);
