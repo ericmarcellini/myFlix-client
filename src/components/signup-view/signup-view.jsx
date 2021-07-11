0d5cb57c24a893637f9077bce595271ab6c837ab
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-07-11 23:54:02 +0200

    signup-view was created!

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+export function SignupView(props){
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ birthday, setBirthday ] = useState('');
+
+
+const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+    <label>
+      Username:
+      <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+    </label>
+    <label>
+      Password:
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+    </label>
+    <button type="submit" onClick={handleSubmit}>Submit</button>
+    <label>
+      Email:
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+    </label>
+    <button type="submit" onClick={handleSubmit}>Submit</button>
+    <label>
+      Birthday:
+      <input type="birthday" value={birthday} onChange={e => setBirthday(e.target.value)} />
+    </label>
+    <button type="submit" onClick={handleSubmit}>Submit</button>
+  </form>
+);  
+}
