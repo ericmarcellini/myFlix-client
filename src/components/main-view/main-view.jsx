405484e61d927bc4bfa34d7b4c1fd04d1360f5c0
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-07-11 23:45:14 +0200

    Initial Commit

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,13 +3,16 @@ import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+
 export class MainView extends React.Component {
 
   constructor(){
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     };
   }
 
@@ -31,9 +34,16 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render(){
       const {movies, selectedMovie} = this.state;
- ///     if (selectedMovie) return <MovieView movie={selectedMovie} />;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+      if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
       if (movies.length === 0) return <div className="main-view" />;
           return (
