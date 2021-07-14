0c6e88b032960d5f04ddf98a41efd6be5a291202
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-07-14 11:59:15 +0200

    Initial commit

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+    return (
+      <form>
+        <Form.Group controlId='formUsername'>
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type='text' onChange={e => setUsername(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group controlId='formPassword'>
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" onChange={e => setPassword(e.target.value)}/>
+        </Form.Group>
+
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+        </Button>
+      </form>
+      );
+}
