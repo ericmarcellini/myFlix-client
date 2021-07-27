eb918c90995d5ee938904021a1dfe1a119030e4f
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-07-27 13:01:32 +0200

    added getUser & proper proptypes for ProfileView

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,28 +1,64 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import {Row, Col, Button, Container, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { Link } from "react-router-dom";
 
 export class ProfileView extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            username: null,
+            password: null,
+            email: null,
+            birthday: null,
+            Favorites: [],
+        }
+    }
+
+    componentDidMount(){
+        let accessToken = localStorage.getItem('token');
+        this.getUser(accessToken);
+    }
+
+    // gets user 
+    getUser(token) {
+        const username = localStorage.getItem('user');
+        axios.get(`https://myflixdb1112.herokuapp.com/users/${username}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+          .then((response) => {
+            this.setState({
+              Username: response.data.Username,
+              Password: response.data.Password,
+              Email: response.data.Email,
+              Birthdate: response.data.Birthdate,
+              FavoriteMovies: response.data.FavoriteMovies,
+            });
+          });
+      }
+
+
     render(){
         const { user, onBackClick } = this.props;
+        const { movies } = this.props;
+        const { FavoriteMovies } = this.props;
 
         return (
-            <div className='profile-view'>
-                <div className='username'>
-                    <span className='label'>Username: </span>
-                    <span className='value'>{user.Name}</span>
-                </div>
-                <div className='x'></div>
-            </div>
+
         )
     }
 }
 
 ProfileView.propTypes = {
     User: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
+        Username: PropTypes.string.isRequired,
+        Email: PropTypes.string.isRequired,
+        Birthdate: PropTypes.string,
+        //FavoriteMovies:
 
     })
 }
+
+
+export default ProfileView;
