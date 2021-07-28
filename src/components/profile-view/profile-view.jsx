9f7115b418dcf0ad7ce73e1ea4b3fd09f904ef34
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-07-28 11:06:26 +0200

    user Update form & handleUpdateUser created

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Button, Container, Card } from 'react-bootstrap';
+import {Row, Col, Button, Container, Card, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { Link } from "react-router-dom";
@@ -59,11 +59,90 @@ export class ProfileView extends React.Component{
         });
  }
 
+    // update user
+    handleUpdateUser(e){
+        this.setState({
+            validated: null
+        });
+
+        const form = e.currentTarget;
+        if (form.checkValidity() === false){
+            e.preventDefault();
+            e.stopPropagation();
+            this.setState({
+                validated: true,
+            });
+            return;
+        } e.preventDefault();
+
+        const username = localStorage.getItem('user');
+        const token = localStorage.getItem('token')
+
+        axios.put(`https://myflixdb1112.herokuapp.com/users/${username}`,{
+                Username: this.state.Username,
+                Password: this.state.Password,
+                Email: this.state.Email,
+                Birthday: this.state.Birthday,
+            },
+            {headers: { Authorization: `Bearer ${token}` }}
+        )
+        .then((response) => {
+            const updatedData = response.updatedData;
+            alert(user + " is updated!")
+            console.log(response.updatedData);
+            console.log(updatedData);
+            console.log(user + " is updated");
+        })
+        .catch(function (error){
+            console.log(error.response.updatedData);
+        });
+    }
+
+    setUsername(input) {
+        this.Username = input;
+    }
+    
+    setPassword(input) {
+        this.Password = input;
+    }
+    
+    setEmail(input) {
+        this.Email = input;
+    }
+    
+    setBirthdate(input) {
+        this.Birthdate = input;
+    }
 
     render(){
         const { user, onBackClick, movies, FavoriteMovies } = this.props;
         return (
+            /* update form */
+        <Form className="updateForm" noValidate validated={validated}  onSubmit={(e) => this.handleUpdate(e, this.Username, this.Password, this.Email, this.Birthdate)}>
+            <Form.Group controlId="formGroupUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter your new username" value={username} onChange={e => this.setUsername(e.target.value)} required/>
+                <Form.Control.Feedback type="invalid">Please provide a valid username.</Form.Control.Feedback>
+            </Form.Group>
 
+            <Form.Group controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="New Password" value={password} onChange={e => setPassword(e.target.value)} required />
+                <Form.Control.Feedback type="invalid">Please provide a valid password.</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="formGroupEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="New Email" value={email} onChange={e => setEmail(e.target.value)} required />
+                <Form.Control.Feedback type="invalid">Please provide a valid email.</Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group controlId="formGroupBirthdate">
+                <Form.Label>Birthdate</Form.Label>
+                <Form.Control type="date" placeholder="00-00-0000" value={birthdate} onChange={e => setBirthdate(e.target.value)} required />
+            </Form.Group> 
+            <Button type="submit"> Submit </Button>
+           </Form> 
         )
     }
 }
@@ -73,7 +152,7 @@ ProfileView.propTypes = {
         Username: PropTypes.string.isRequired,
         Email: PropTypes.string.isRequired,
         Birthdate: PropTypes.string,
-        //FavoriteMovies:
+        FavoriteMovies: PropTypes.array,
 
     })
 }
